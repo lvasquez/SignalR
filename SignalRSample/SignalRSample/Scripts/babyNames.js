a0602cc72131733f2439aafb93ec1f5df4ccdbbc
Luis Ernesto Vasquez <levb20@gmail.com>
2013-08-20 22:41:46 -0600

    First Commit
    SignalR Sample

---
+++ SignalRSample/SignalRSample/Scripts/babyNames.js
@@ -0,0 +1,20 @@
+﻿/// <reference path="jquery-2.0.3.js" />
+/// <reference path="jquery.signalR-1.0.0.js" />
+
+$(document).ready(function () {
+    var hub = $.connection.babyNamesHub,
+        babyNamesTable = $('#BabyNamesTable'),
+        babyName = $('#BabyName');
+
+    hub.client.babyNamed = function (name) {
+        babyNamesTable.append('<tr><td>' + name + '</td></tr>');
+        babyName.val('');
+    };
+
+    $.connection.hub.start().done(function () {
+        $('#AddButton').click(function () {
+            var name = babyName.val();
+            hub.server.addBabyName(name);
+        });
+    });
+});
